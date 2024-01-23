f8cb40177ef0bcdb9848f6e0c37ee6524753ae4c
SoumapriyoM <21f3001571@student.onlinedegree.iitm.ac.in>
2024-01-23 16:22:11 +0530

    requirement & installation done

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
+REPO_NAME = "MyBandhubot"
 AUTHOR_USER_NAME = "SoumapriyoM"
 SRC_REPO = "MyBandhubot"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "soumapriyomondalabc@gmail.com"
 
 
 
